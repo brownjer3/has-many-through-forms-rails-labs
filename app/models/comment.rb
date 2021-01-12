6db169e739a1905f8049b8adc4699ee3af7aea95
jerry <brownjer3@gmail.com>
2021-01-12 09:38:09 -0800

    commit tree b0cf5391f000a57d96df2651b9a87f151fd94523

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,10 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  accepts_nested_attributes_for :user, reject_if: proc { |attributes| attributes['username'].blank? }
+
+  def user_says_comment
+    "#{self.user.username} says: #{self.content}"
+  end
+
 end
